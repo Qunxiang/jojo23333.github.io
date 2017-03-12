33647a1ec7c1a03e54729a0bf7729e7a371b3894
U-DESKTOP-IKP9AMK\77936 <jonassenli97@gmail.com>
2017-03-12 14:51:36 +0800

    using self made deploy instead

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 rm -rf public
-git clone git@github.com:jojo23333/jojo23333.github.io.git ./public
+if [ ! -d "../jojo23333.github.io" ]; then
+git clone git@github.com:jojo23333/jojo23333.github.io.git ..
+fi
 hexo clean
 hexo g
-cd ./public
+cd  ../jojo23333.github.io
+git pull
+mv  -rf ../Blog/public/*  ./
 git add -A
 git commit -m"Update"
 git push origin master
